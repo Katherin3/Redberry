4f0bc83d447bc68860a36aa7d3f1ab5bae194d44
Eka Gadakhabadze <eka.gadaxabadze@gmail.com>
2022-07-16 14:38:57 +0400

    migrations and models

---
+++ hr-soft/app/Models/Candidate.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Candidate extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->first_name;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->last_name;
+    }
+
+    public function getPosition(): string
+    {
+        return $this->position;
+    }
+
+    public function getMinSalary(): int
+    {
+        return $this->min_salary;
+    }
+
+    public function getMaxSalary(): int
+    {
+        return $this->max_salary;
+    }
+
+    public function getLinkedinUrl(): int
+    {
+        return $this->linkedin_url;
+    }
+
+    public function skills(): BelongsToMany
+    {
+        return $this->belongsToMany(Skill::class);
+    }
+}
