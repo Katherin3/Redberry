d96242b2272cc86485b9275e3b88f303fb23f5a3
Eka Gadakhabadze <eka.gadaxabadze@gmail.com>
2022-07-16 22:53:12 +0400

    install spatie query builder and use in repository

--- hr-soft/app/Repositories/CandidatesRepository.php
+++ hr-soft/app/Repositories/CandidatesRepository.php
@@ -8,26 +8,44 @@ use App\DataTransferObjects\UpdateCandidatesDTO;
 use App\Interfaces\CandidatesRepositoryInterface;
 use App\Models\Candidate;
 use Illuminate\Database\Eloquent\Collection;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class CandidatesRepository implements CandidatesRepositoryInterface
 {
     public function find(int $id): ?Candidate
     {
-        //
+        return QueryBuilder::for($this->getModel())->find($id);
     }
 
     public function findMany(GetCandidatesDTO $dto): Collection
     {
-        return Candidate::all();
+        $candidates = QueryBuilder::for($this->getModel())->get();
+
+        $candidates->load('skills');
+
+        return $candidates;
     }
 
     public function create(CreateCandidatesDTO $dto): Candidate
     {
-        //
+        $candidate = QueryBuilder::for($this->getModel())->create($dto->getValues());
+
+        $candidate->load('skills');
+
+        return $candidate;
     }
 
     public function update(Candidate $candidate, UpdateCandidatesDTO $dto): Candidate
     {
-        //
+        $candidate->update($dto->getValues());
+
+        $candidate->load('skills');
+
+        return $candidate;
+    }
+
+    public function getModel(): Candidate
+    {
+        return new Candidate();
     }
 }
