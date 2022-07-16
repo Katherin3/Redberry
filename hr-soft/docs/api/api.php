bdd149dd225bf30d7b752d2e65ad5ff77dced78e
Eka Gadakhabadze <eka.gadaxabadze@gmail.com>
2022-07-16 18:25:31 +0400

    swagger

---
+++ hr-soft/docs/api/api.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @OA\Info(
+ *     title=L5_SWAGGER_CONST_TITLE,
+ *     description=L5_SWAGGER_CONST_DESCRIPTION,
+ *     version="1.0.0",
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ */
+
+/**
+ *  @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="API Server"
+ *  )
+ */
+
+/**
+ * @OA\Tag(
+ *     name="Authentication",
+ *     description="Everything about Authentication",
+ * )
+ */
